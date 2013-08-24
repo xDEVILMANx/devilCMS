9c46bec6fb03f65e62c318b10275612dda54c03e
xDEVILMANx <falko@webwenta.de>
2013-08-25 00:34:26 +0200

    Update devilCMS.elements.js

--- devilCMS.elements.js
+++ devilCMS.elements.js
@@ -1,22 +1,22 @@
 devilCMS.elements = {
 	init: function(callback){
 		var me = this;
-			me.container 		= $('#'+devilCMS.settings.ELEMENTS.ID.CONTAINER);
-			me.header 			= $('#'+devilCMS.settings.ELEMENTS.ID.HEADER);
-			me.navigation.left 	= $('#'+devilCMS.settings.ELEMENTS.ID.NAVIGATION.LEFT);
-			me.navigation.right = $('#'+devilCMS.settings.ELEMENTS.ID.NAVIGATION.RIGHT);
-			me.navigation.top 	= $('#'+devilCMS.settings.ELEMENTS.ID.NAVIGATION.TOP);
-			me.mainframe 		= $('#'+devilCMS.settings.ELEMENTS.ID.MAINFRAME);
-			me.content 			= $('#'+devilCMS.settings.ELEMENTS.ID.CONTENT);
-			me.footer 			= $('#'+devilCMS.settings.ELEMENTS.ID.NAVIGATION.BOTTOM);
-			
-			me.spinner = $('<div />')
+		me.container 		= $('#'+devilCMS.settings.ELEMENTS.ID.CONTAINER);
+		me.header 		= $('#'+devilCMS.settings.ELEMENTS.ID.HEADER);
+		me.navigation.left 	= $('#'+devilCMS.settings.ELEMENTS.ID.NAVIGATION.LEFT);
+		me.navigation.right 	= $('#'+devilCMS.settings.ELEMENTS.ID.NAVIGATION.RIGHT);
+		me.navigation.top 	= $('#'+devilCMS.settings.ELEMENTS.ID.NAVIGATION.TOP);
+		me.mainframe 		= $('#'+devilCMS.settings.ELEMENTS.ID.MAINFRAME);
+		me.content 		= $('#'+devilCMS.settings.ELEMENTS.ID.CONTENT);
+		me.footer 		= $('#'+devilCMS.settings.ELEMENTS.ID.NAVIGATION.BOTTOM);
+		
+		me.spinner = $('<div />')
 			.attr('id', 'siteLoadingIndicator')
 			.addClass('ui-widge ui-widget-header ui-corner-all')
 			.text('Please wait...')
 			.appendTo('body');
-			
-			if(typeof(callback) == 'function') callback();
+		
+		if(typeof(callback) == 'function') callback();
 	},
 	container : null,
 	header: null,
